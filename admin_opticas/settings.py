54831ac756a342026d4138d20d9827d51dbac4c8
jvasquez <jvasquez@softlabperu.com>
2024-10-02 19:17:38 -0500

    fix media root

--- admin_opticas/settings.py
+++ admin_opticas/settings.py
@@ -139,8 +139,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = '/app/static'
-MEDIA_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', None)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
