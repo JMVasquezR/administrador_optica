93865942e664d5dfb2cda49700df2df87dcc023c
MVasquez <josemartivr@gmail.com>
2026-03-07 17:23:45 -0500

    se elimina archivo

--- admin_opticas/settings.py
+++ admin_opticas/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Para producción con collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Para producción con collectstatic
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') if os.getenv('MEDIA_ROOT') is None else os.getenv('MEDIA_ROOT')
 
 STATICFILES_DIRS = [
